c3ad29d28d1096afbbe87cbf2a13268c3d33f025
Kang Ji <rkdwldms9058@naver.com>
2026-08-12 20:53:28 +0900

    Arrange monthly cards in horizontal gallery

--- scripts/process_design_images.py
+++ scripts/process_design_images.py
@@ -28,6 +28,8 @@ def save(image: Image.Image, name: str) -> None:
 card = Image.open(DESIGN / "card_news.jpg").convert("RGB")
 card_rows = [card.crop(box) for box in [(28, 108, 790, 298), (28, 497, 790, 687), (28, 883, 790, 1074), (28, 1270, 790, 1461), (28, 1652, 790, 1843)]]
 save(polish(stack(card_rows, 26)), "card_news-clean.jpg")
+for month, row in zip(("06", "05", "04", "03", "02"), card_rows):
+    save(polish(row), f"card-news-{month}.jpg")
 
 banner = Image.open(DESIGN / "banner&popup.jpg").convert("RGB")
 save(polish(stack([banner.crop((33, 112, 784, 512)), banner.crop((33, 719, 784, 1459))], 30)), "banner-popup-clean.jpg")
